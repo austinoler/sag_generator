2cac204dd6d455cf0c96fc80e15c37c63230e832
Austin Oler <oleraustin@gmail.com>
2023-11-28 20:35:46 -0500

    created files and started logo.js

---
+++ logo.js
@@ -0,0 +1,14 @@
+class Logo {
+    constructor(text, textColor, shape, shapeColor) {
+      this.text = text;
+      this.textColor = textColor;
+      this.shape = shape;
+      this.shapeColor = shapeColor;
+    }
+  
+    generateSVG() {
+      // Implement SVG generation logic here
+    }
+  }
+  
+  module.exports = Logo;
