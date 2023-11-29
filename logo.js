a9109b41ccff82e7e685187744b16af4eed6eb1f
Austin Oler <oleraustin@gmail.com>
2023-11-29 14:20:07 -0500

    created generateSVG function

--- logo.js
+++ logo.js
@@ -7,8 +7,32 @@ class Logo {
     }
   
     generateSVG() {
-      // Implement SVG generation logic here
-    }
-  }
   
-  module.exports = Logo;
+    const width = 300;
+    const height = 200;
+
+    // SVG opening tag with width and height attributes
+    let svgContent = `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">`;
+
+    // Text element
+    svgContent += `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`;
+
+    // Square element)
+    if (this.shape === 'square') {
+      svgContent += `<rect width="80%" height="80%" x="10%" y="10%" fill="${this.shapeColor}" />`;
+    } else if (this.shape === 'circle') {
+      // Circle element
+      svgContent += `<circle cx="50%" cy="50%" r="40%" fill="${this.shapeColor}" />`;
+    } else if (this.shape === 'triangle') {
+      // Triangle element
+      svgContent += `<polygon points="50%,10% 90%,90% 10%,90%" fill="${this.shapeColor}" />`;
+    }
+
+    // SVG closing tag
+    svgContent += `</svg>`;
+
+    return svgContent;
+  }
+}
+
+module.exports = Logo;
