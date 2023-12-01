2c35528998c062349046b5e5e638f28e4d9df6ac
Austin Oler <oleraustin@gmail.com>
2023-11-30 19:35:52 -0500

    implented createSVG function

--- utils.js
+++ utils.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
 function createSVG(content) {
+  const svgDeclaration = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>';
+  const svgOpeningTag = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ' +
+    '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
   
+  const svgContent = `${svgDeclaration}\n${svgOpeningTag}\n${content}`;
+  
+  return svgContent;
 }
 
 function saveSVG(content, filename) {
